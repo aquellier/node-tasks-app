981f3afe219d02f6dab06cecc7f917198f4e513f
antoinequellier <antoinecquellier@gmail.com>
2019-08-23 20:57:45 +0200

    creating a collection and inserting a document

---
+++ mongodb.js
@@ -0,0 +1,23 @@
+// CRUD
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log('Unable to connect to DB')
+  }
+  console.log('Connected correctly')
+
+  const db = client.db(databaseName)
+
+  db.collection('users').insertOne({
+    name: 'Antoine',
+    age: 27
+  })
+})
+
+
+
